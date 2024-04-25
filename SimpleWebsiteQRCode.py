d2bb14d0aaab51cccde7d18a6fa4a7840b904888
Patrick Adams <patrickadams19@hotmail.com>
2024-04-25 15:52:52 -0400

    initial commit

---
+++ SimpleWebsiteQRCode.py
@@ -0,0 +1,21 @@
+import qrcode 
+import qrcode.image.svg
+import os
+
+
+def get_output_location():
+    directory = input("Input folder path for QR Code output: ")
+    fileName = input("Input a filename for QR Code: ").split(".")[0]
+    
+
+    return os.path.join(directory,fileName)
+    
+if __name__ == "__main__":
+    outputPath = get_output_location() + ".png"
+    qr = qrcode.QRCode(version=10)
+    websiteUrl = input("Insert URL for website: ")
+    qr.add_data(websiteUrl)
+    qr.make()
+    image = qr.make_image()
+    image.save(outputPath)
+
